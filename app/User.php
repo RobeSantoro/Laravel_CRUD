e4936a0de03df364fb292cd6ccc5fca50e727f70
Robe Santoro <robe.santoro@live.com>
2020-06-24 22:27:41 +0200

    Added relation hasMany

--- app/User.php
+++ app/User.php
@@ -16,7 +16,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'role'
     ];
 
     /**
@@ -36,4 +39,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function reservations()
+    {
+        return $this->hasMany('App\Reservation')
+    }
+
 }
