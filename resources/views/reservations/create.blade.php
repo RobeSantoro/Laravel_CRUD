62f4522d07dc7496295ae80596e9cc13ac5bfa7b
Robe Santoro <robe.santoro@live.com>
2020-06-26 00:12:55 +0200

    Updated Validation error display

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -3,46 +3,27 @@
 
 <div class="container mt-5">
 
-    <div class="row">
-        <div class="col-12">
-            @if($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12">
-            @if(session('message'))
-                <div class="alert alert-success">
-                    {{ session('message') }}
-                </div>
-            @endif
-        </div>
-    </div>
-
-
-    <div class="row">
-        <div class="col-12">
+    <div class="row justify-content-center">
+        <div class="col-8">
             <form action="{{ route('reservations.store') }}" method="POST">
                 @csrf
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Data</label>
+                <div class="form-group m-0 mt-2">
+                    <label class="m-0" for="exampleInputEmail1">Data</label>
                     <input type="date" name="data" class="form-control" id="exampleInputEmail1"
                         aria-describedby="emailHelp">
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Trattamento</label>
+                @if($errors->has('data'))
+                    <p style="color:red;">{{ $errors->first('data') }}</p>
+                @endif
+                <div class="form-group m-0 mt-2">
+                    <label class="m-0" for="exampleInputEmail1">Trattamento</label>
                     <input type="text" name="trattamento" class="form-control" id="exampleInputEmail1"
                         aria-describedby="emailHelp">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                @if($errors->has('trattamento'))
+                    <p style="color:red;">{{ $errors->first('trattamento') }}</p>
+                @endif
+                <button type="submit" class="btn btn-primary mt-4">Submit</button>
             </form>
         </div>
     </div>
