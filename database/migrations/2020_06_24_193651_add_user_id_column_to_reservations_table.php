c61c9e5db1f6efc3567c8fec00c44c6bfc2a929a
Robe Santoro <robe.santoro@live.com>
2020-06-30 22:13:40 +0200

    Added  ->nullable() to foreign('user_id')

--- database/migrations/2020_06_24_193651_add_user_id_column_to_reservations_table.php
+++ database/migrations/2020_06_24_193651_add_user_id_column_to_reservations_table.php
@@ -15,7 +15,7 @@ class AddUserIdColumnToReservationsTable extends Migration
     {
         Schema::table('reservations', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->nullable();
         });
     }
 
