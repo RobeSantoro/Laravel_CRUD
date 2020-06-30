08184ae8b023be3890d6e00631d4172b825faf21
Robe Santoro <robe.santoro@live.com>
2020-06-30 12:22:33 +0200

    Created Migration Many to Many

---
+++ database/migrations/2020_06_30_101515_create_doctor_reservation_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_reservation', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('doctor_id')->nullable();
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+
+            $table->unsignedBigInteger('reservation_id')->nullable();
+            $table->foreign('reservation_id')->references('id')->on('reservations');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_reservation');
+    }
+}
