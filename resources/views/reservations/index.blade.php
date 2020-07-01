823e51bf22b725399bf07af7ddd712d0467bf665
Robe Santoro <robe.santoro@live.com>
2020-07-01 15:46:38 +0200

    Deleted Validation Div

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -3,20 +3,6 @@
 
 <div class="container mt-5">
 
-    <div class="row">
-        <div class="col-12">
-            @if($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-    </div>
-
     {{-- Come fare a mandare una stringa per sostituire una classe css a seconda se abbiamo eliminato o creato? --}}
     <div class="row">
         <div class="col-12">
