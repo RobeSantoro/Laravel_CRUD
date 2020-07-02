6e57db2a7178b16248f4e4c48bcaf3833d07f0f8
Robe Santoro <robe.santoro@live.com>
2020-07-02 20:49:55 +0200

    Added Select

--- resources/views/reservations/edit.blade.php
+++ resources/views/reservations/edit.blade.php
@@ -3,33 +3,8 @@
 
 <div class="container mt-5">
 
-    <div class="row">
-        <div class="col-12">
-            @if($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12">
-            @if(session('message'))
-                <div class="alert alert-success">
-                    {{ session('message') }}
-                </div>
-            @endif
-        </div>
-    </div>
-
-
-    <div class="row">
-        <div class="col-12">
+    <div class="row justify-content-center">
+        <div class="col-8">
             <form action="{{ route('reservations.update', compact('reservation')) }}" method="POST">
                 @method('PUT')
                 @csrf
@@ -38,11 +13,29 @@
                     <label for="3">Data</label>
                     <input type="date" name="data" class="form-control" id="3" value="{{ $reservation->data, old('data') }}">
                 </div>
-                <div class="form-group">
-                    <label for="4">Trattamento</label>
-                    <input type="text" name="trattamento" class="form-control" id="4" value="{{ $reservation->trattamento, old('trattamento') }}">
+
+                @if($errors->has('data'))
+                    <p style="color:red;">{{ $errors->first('data') }}</p>
+                @endif
+
+                <div class="form-group m-0 mt-2">
+                    <label for="role" class="mb-0">Trattamento</label>
+
+                    <div class="col-12 px-0">
+                        <select id="role" class="form-control" name="trattamento">
+                            {{-- Sarà il @foreach della table Trattamenti --}}
+                            <option>Carie</option>
+                            <option>Pulizia</option>
+                            <option>Dentiera</option>
+                        </select>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Modifica</button>
+
+                @if($errors->has('trattamento'))
+                    <p style="color:red;">{{ $errors->first('trattamento') }}</p>
+                @endif
+
+                <button type="submit" class="btn btn-primary my-3">Modifica</button>
 
             </form>
         </div>
